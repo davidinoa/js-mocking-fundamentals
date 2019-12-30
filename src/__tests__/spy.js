30486ce4b94ce69ae75b2151766b09c521acc140
David Inoa <davidinoa@gmail.com>
2019-12-30 07:51:57 -0500

    Add my solutions

--- src/__tests__/spy.js
+++ src/__tests__/spy.js
@@ -1,17 +1,19 @@
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const thumbWar = require('../thumb-war');
+const utils = require('../utils');
 
 test('returns winner', () => {
-  jest.spyOn(utils, 'getWinner')
-  utils.getWinner.mockImplementation((p1, p2) => p1)
+  jest.spyOn(utils, 'getWinner');
+  utils.getWinner.mockImplementation((p1, p2) => p1);
 
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
+  const winner = thumbWar('David', 'Daniel');
+
+  expect(winner).toBe('David');
+
   expect(utils.getWinner.mock.calls).toEqual([
-    ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler']
-  ])
+    ['David', 'Daniel'],
+    ['David', 'Daniel'],
+  ]);
 
   // cleanup
-  utils.getWinner.mockRestore()
-})
+  utils.getWinner.mockRestore();
+});
--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,12 +1,11 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require('assert');
+const thumbwar = require('../thumb-war');
+const utils = require('../utils');
 
-const originalGetWinner = utils.getWinner
-utils.getWinner = (p1, p2) => p1
+const originalGetWinner = utils.getWinner;
+utils.getWinner = (p1, p2) => p1;
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbwar('David', 'Daniel');
+assert.strictEqual(winner, 'David');
 
-// cleanup
-utils.getWinner = originalGetWinner
+utils.getWinner = originalGetWinner;
